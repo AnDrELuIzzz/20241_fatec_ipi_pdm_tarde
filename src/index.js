fa74cb7bf591fe1ba237b229bca75ffcbb0776c6
Andre Luiz <luizzz.z127@gmail.com>
2024-02-23 18:05:38 -0300

    feat(props): exibe pedidos sem componentes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return (
+      <div className="container border rounded mt-2">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="display-5 text-center"> Seus pedidos</h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12 col-lg-4">
+            <div className="card">
+              <div className="card-header text-muted">20/04/2023</div>
+              <div className="card-body d-flex">
+                <div d-flex align-items-center>
+                  <i className="fa-solid fa-house fa-2x"></i>
+                </div>
+                <div className=" flex-grow-1 ms-2 border">
+                  <h4 className="text-center">Livro</h4>
+                  <p>Concret Mathematics - Donald Kunth</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-lg-4">
+            <div className="card">
+              <div className="card-header text-muted">20/04/2023</div>
+              <div className="card-body d-flex">
+                <div d-flex align-items-center>
+                  '<i class="fa-solid fa-gamepad  fa-2x"></i>
+                </div>
+                <div className=" flex-grow-1 ms-2 border">
+                  <h4 className="text-center">Gamepad</h4>
+                  <p>Ipega</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-lg-4">
+            <div className="card">
+              <div className="card-header text-muted">20/04/2023</div>
+              <div className="card-body d-flex">
+                <div d-flex align-items-center>
+                  <i class="fa-solid fa-laptop fa-2x"></i>
+                </div>
+                <div className=" flex-grow-1 ms-2 border">
+                  <h4 className="text-center">Laptop</h4>
+                  <p>Lenovo Ideapad S145</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
+
